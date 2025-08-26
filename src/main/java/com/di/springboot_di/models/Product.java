ae5dbc3a0763c071469981296eeb37bae53e8df8
ahxdevelopmx <91965647+ahxdevelopmx@users.noreply.github.com>
2025-08-26 15:34:26 -0600

    first commit

---
+++ src/main/java/com/di/springboot_di/models/Product.java
@@ -0,0 +1,33 @@
+package com.di.springboot_di.models;
+
+public class Product {
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    private Long id;
+    private String name;
+    private Long price;
+
+
+}
